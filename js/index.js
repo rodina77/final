b2caf2259341081ecd02fd4d5d1e76fb0e008763
Rodina Ibrahim <99216903+rodina77@users.noreply.github.com>
2022-03-08 22:47:28 +0200

    version-1

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+document.querySelector('.list').addEventListener('click', () => {
+    document.querySelector(".nav").classList.toggle("active");
+    });
+
+    document.querySelector('.fa-bars').addEventListener('click', () => {
+        document.querySelector(".navit").classList.toggle("active");
+        });
